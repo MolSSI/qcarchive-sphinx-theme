77068b778f1eaa526d11dc9bd0125be13e98f840
Daniel Smith <malorian@me.com>
2019-03-03 10:51:36 -0500

    Replace RTD exactly, but with custom CSS overrides

---
+++ qcarchive_sphinx_theme/__init__.py
@@ -0,0 +1,14 @@
+from os import path
+
+def get_html_theme_path():
+	    """Return list of HTML theme paths."""
+	    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+	    return cur_dir
+
+# See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+def setup(app):
+    app.add_html_theme('qcarchive_sphinx_theme', path.abspath(path.dirname(__file__)))
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
